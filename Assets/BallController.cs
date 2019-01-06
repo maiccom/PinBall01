e66947b4706cb26641781419cdd42c58b8573408
maiccom <maiko.matsuda@gmail.com>
2019-01-06 21:03:08 +0900

    PinBallプロジェクト　再提出(2019年1月6日)
    前回コミットがうまくいかず、PinBallプロジェクトのゲームシーンが消えてしまったので、ゲームシーンを一から作り直しました。

---
+++ Assets/BallController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    //スコア
+    private int score;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+        //ScoreTextにスコアを表示
+        this.scoreText.GetComponent<Text>().text = "SCORE:" + this.score;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over!!";
+        }
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        // タグによって加算するスコアを決める
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.score += 15;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 25;
+        }
+
+        //ScoreTextにスコアを表示
+        this.scoreText.GetComponent<Text>().text = "SCORE:" + this.score;
+    }
+
+
+}
